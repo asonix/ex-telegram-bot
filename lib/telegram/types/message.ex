6bbff71f8421015ec57fd185b6ce18f8175350ee
Riley Trautman <asonix@tamu.edu>
2017-04-02 15:23:02 -0500

    Fix types

--- lib/telegram/types/message.ex
+++ lib/telegram/types/message.ex
@@ -3,113 +3,49 @@ defmodule Telegram.Types.Message do
   This struct represents message data provided by Telegram's bot API
   """
 
-  @type input_type :: %{
-    required(String.t) => integer,
-    optional(String.t) => Telegram.Types.User.input_type,
-    required(String.t) => integer,
-    optional(String.t) => Telegram.Types.Chat.input_type,
-    optional(String.t) => Telegram.Types.User.input_type,
-    optional(String.t) => Telegram.Types.Chat.input_type,
-    optional(String.t) => integer,
-    optional(String.t) => integer,
-    optional(String.t) => __MODULE__.input_type,
-    optional(String.t) => integer,
-    optional(String.t) => String.t,
-    optional(String.t) => [Telegram.Types.MessageEntity.input_type],
-    optional(String.t) => Telegram.Types.Audio.input_type,
-    optional(String.t) => Telegram.Types.Document.input_type,
-    optional(String.t) => Telegram.Types.Game.input_type,
-    optional(String.t) => [Telegram.Types.PhotoSize.input_type],
-    optional(String.t) => Telegram.Types.Sticker.input_type,
-    optional(String.t) => Telegram.Types.Video.input_type,
-    optional(String.t) => Telegram.Types.Voice.input_type,
-    optional(String.t) => String.t,
-    optional(String.t) => Telegram.Contact.input_type,
-    optional(String.t) => Telegram.Location.input_type,
-    optional(String.t) => Telegram.Venue.input_type,
-    optional(String.t) => Telegram.Types.User.input_type,
-    optional(String.t) => Telegram.Types.User.input_type,
-    optional(String.t) => String.t,
-    optional(String.t) => [Telegram.Types.PhotoSize.input_type],
-    optional(String.t) => boolean,
-    optional(String.t) => boolean,
-    optional(String.t) => boolean,
-    optional(String.t) => boolean,
-    optional(String.t) => integer,
-    optional(String.t) => integer,
-    optional(String.t) => __MODULE__.input_type
-  }
+  @type input_type :: Telegram.Types.Message.Audio.input_type |
+                      Telegram.Types.Message.ChannelChatCreated.input_type |
+                      Telegram.Types.Message.ChatMigration.input_type |
+                      Telegram.Types.Message.Contact.input_type |
+                      Telegram.Types.Message.DeleteChatPhoto.input_type |
+                      Telegram.Types.Message.Document.input_type |
+                      Telegram.Types.Message.Game.input_type |
+                      Telegram.Types.Message.GroupChatCreated.input_type |
+                      Telegram.Types.Message.LeftChatMember.input_type |
+                      Telegram.Types.Message.Location.input_type |
+                      Telegram.Types.Message.NewChatMember.input_type |
+                      Telegram.Types.Message.NewChatPhoto.input_type |
+                      Telegram.Types.Message.NewChatTitle.input_type |
+                      Telegram.Types.Message.PhotoSize.input_type |
+                      Telegram.Types.Message.PinnedMessage.input_type |
+                      Telegram.Types.Message.Sticker.input_type |
+                      Telegram.Types.Message.SupergroupChatCreated.input_type |
+                      Telegram.Types.Message.Text.input_type |
+                      Telegram.Types.Message.Venue.input_type |
+                      Telegram.Types.Message.Video.input_type |
+                      Telegram.Types.Message.Voice.input_type
 
-  @type t :: %__MODULE__{
-              message_id:               integer,
-              from:                     Telegram.Types.User.t | nil,
-              date:                     integer,
-              chat:                     Telegram.Types.Chat.t | nil,
-              forward_from:             Telegram.Types.User.t | nil,
-              forward_from_chat:        Telegram.Types.Chat.t | nil,
-              forward_from_message_id:  integer | nil,
-              forward_date:             integer | nil,
-              reply_to_message:         __MODULE__.t | nil,
-              edit_date:                integer | nil,
-              text:                     String.t | nil,
-              entities:                 [Telegram.Types.MessageEntity.t],
-              audio:                    Telegram.Types.Audio.t | nil,
-              document:                 Telegram.Types.Document.t | nil,
-              game:                     Telegram.Types.Game.t | nil,
-              photo:                    [Telegram.Types.PhotoSize.t],
-              sticker:                  Telegram.Types.Sticker.t | nil,
-              video:                    Telegram.Types.Video.t | nil,
-              voice:                    Telegram.Types.Voice.t | nil,
-              caption:                  String.t | nil,
-              contact:                  Telegram.Contact.t | nil,
-              location:                 Telegram.Location.t | nil,
-              venue:                    Telegram.Venue.t | nil,
-              new_chat_member:          Telegram.Types.User.t | nil,
-              left_chat_member:         Telegram.Types.User.t | nil,
-              new_chat_title:           String.t | nil,
-              new_chat_photo:           [Telegram.Types.PhotoSize.t],
-              delete_chat_photo:        boolean,
-              group_chat_created:       boolean,
-              supergroup_chat_created:  boolean,
-              channel_chat_created:     boolean,
-              migrate_to_chat_id:       integer | nil,
-              migrate_from_chat_id:     integer | nil,
-              pinned_message:           __MODULE__.t | nil}
-
-  defstruct message_id:               0,
-            from:                     nil,
-            date:                     0,
-            chat:                     nil,
-            forward_from:             nil,
-            forward_from_chat:        nil,
-            forward_from_message_id:  nil,
-            forward_date:             nil,
-            reply_to_message:         nil,
-            edit_date:                nil,
-            text:                     nil,
-            entities:                 [],
-            audio:                    nil,
-            document:                 nil,
-            game:                     nil,
-            photo:                    [],
-            sticker:                  nil,
-            video:                    nil,
-            voice:                    nil,
-            caption:                  nil,
-            contact:                  nil,
-            location:                 nil,
-            venue:                    nil,
-            new_chat_member:          nil,
-            left_chat_member:         nil,
-            new_chat_title:           nil,
-            new_chat_photo:           [],
-            delete_chat_photo:        false,
-            group_chat_created:       false,
-            supergroup_chat_created:  false,
-            channel_chat_created:     false,
-            migrate_to_chat_id:       nil,
-            migrate_from_chat_id:     nil,
-            pinned_message:           nil
+  @type t :: Telegram.Types.Message.Audio.t |
+             Telegram.Types.Message.ChannelChatCreated.t |
+             Telegram.Types.Message.ChatMigration.t |
+             Telegram.Types.Message.Contact.t |
+             Telegram.Types.Message.DeleteChatPhoto.t |
+             Telegram.Types.Message.Document.t |
+             Telegram.Types.Message.Game.t |
+             Telegram.Types.Message.GroupChatCreated.t |
+             Telegram.Types.Message.LeftChatMember.t |
+             Telegram.Types.Message.Location.t |
+             Telegram.Types.Message.NewChatMember.t |
+             Telegram.Types.Message.NewChatPhoto.t |
+             Telegram.Types.Message.NewChatTitle.t |
+             Telegram.Types.Message.PhotoSize.t |
+             Telegram.Types.Message.PinnedMessage.t |
+             Telegram.Types.Message.Sticker.t |
+             Telegram.Types.Message.SupergroupChatCreated.t |
+             Telegram.Types.Message.Text.t |
+             Telegram.Types.Message.Venue.t |
+             Telegram.Types.Message.Video.t |
+             Telegram.Types.Message.Voice.t
 
   @doc """
   Creates a Message struct from a map object.
